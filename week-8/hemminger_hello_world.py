d2789c67af0b267cc6a8d01f41de2ab84e20004d
Andrew Hemminger <arhemminger@gmail.com>
2019-06-23 13:43:34 -0500

    Adding assignment 8.2 anaconda

---
+++ week-8/hemminger_hello_world.py
@@ -0,0 +1,10 @@
+"""
+============================================
+; Title:  Anaconda
+; Author: Professor Krasso
+; Date:  23 June 2019
+; Modified by: Andrew Hemminger
+; Description: Exercise 8.2 – Anaconda
+;===========================================
+"""
+print('Hello World')
